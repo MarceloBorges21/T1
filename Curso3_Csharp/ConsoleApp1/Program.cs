c7ee4802ad34337b07f9fc36c77d18f23c331b3d
Estagiario 030 <stag030@benner.com.br>
2019-05-21 13:55:30 -0300

    Primeiro commit

---
+++ Curso3_Csharp/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ConsoleApp1.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+            {
+                carlos.Nome = "Carlos";
+                carlos.CPF = "123";
+                carlos.Salario = 2000;
+
+            }
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine("R$"+carlos.Salario);
+            Console.ReadKey();
+        }
+    }
+}
