fb3d756ac5cb57d7c2dcf6dd931c8054b10b5f6a
Estagiario 030 <stag030@benner.com.br>
2019-05-21 15:01:15 -0300

    Herança2

---
+++ Curso3_Csharp/ConsoleApp1/GerenciadorBonificacao.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar (Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
