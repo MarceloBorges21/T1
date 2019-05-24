88c3c623747d6fef9820a60b59ed7e25b33cb9f2
Marcelo Borges <ce.lo.borges@hotmail.com>
2019-05-23 21:30:39 -0300

    conhecendo exceções, tratamento de exceções

--- Curso4_Csharp/ByteBank/Program.cs
+++ Curso4_Csharp/ByteBank/Program.cs
@@ -6,15 +6,52 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ContaCorrente conta = new ContaCorrente(7456, 8907650);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Metodo();
+			}
+			catch (Exception erro)
+			{
+				Console.WriteLine(erro.Message);
+				Console.WriteLine(erro.StackTrace);
+				Console.WriteLine("Aconteceu um erro!");
+			}
+		
+			Console.ReadLine();
+		}
 
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+		// Teste com a cadeia de chamada:
+		// Metodo -> TestaDivisao -> Dividir
+		private static void Metodo()
+		{
+			TestaDivisao(0);
+		}
 
-            Console.ReadLine();
-        }
-    }
+		private static void TestaDivisao(int divisor)
+		{
+			try
+			{
+				int resultado = Dividir(10, divisor);
+				Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+			}
+			catch (DivideByZeroException erro)
+			{
+				Console.WriteLine(erro.Message);
+				Console.WriteLine(erro.StackTrace);
+				Console.WriteLine("Não é possível fazer uma divisão por 0!");
+			}
+		}
+
+		private static int Dividir(int numero, int divisor)
+		{
+			ContaCorrente conta = null;
+			//Console.WriteLine(conta.Saldo);
+
+			return numero / divisor;
+		}
+	}
 }
