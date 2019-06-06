8f62e1ca4218a68f01f196c32d4ca0af26d1c8f8
Estagiario 030 <stag030@benner.com.br>
2019-06-06 16:54:44 -0300

    Fim curso asp.net

---
+++ 8-AspNet_Alura/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        [Route("categoria")]
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categoria = dao.Lista();
+            ViewBag.Categorias = categoria;
+            
+            return View();
+        }
+        
+        public ActionResult cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 8-AspNet_Alura/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Filtros;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    [AutorizacaoFilter]
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        [Route("produtos", Name = "ListaProdutos")]       
+        public ActionResult Index()
+        {      
+                ProdutosDAO dao = new ProdutosDAO();
+                IList<Produto> produtos = dao.Lista();
+                return View(produtos);
+            }
+
+        
+        public ActionResult Cadastro()//para aparecer o id no drop do cadastro de produtos
+        {
+            ViewBag.Produto = new Produto();
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido", "Produtos da categoria informática devem ter preço maior do que 100");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Cadastro");
+            }
+        }
+
+        [Route("produtos/{id}", Name ="VisualizaProduto")]
+        public ActionResult Visualiza(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            ViewBag.Produto = produto;
+            return View();
+        }
+
+        public ActionResult DecrementaQtd(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            produto.Quantidade--;
+            dao.Atualiza(produto);
+            return Json(produto);
+        }
+
+    }
+}
